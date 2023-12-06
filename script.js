983559dfdce53dc80cae2d97167a4b4a2d795f3e
Trey <chadktracy@gmail.com>
2023-12-06 06:17:17 -0600

    Testing playlist

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
-// Document has been loaded
-$( document ).ready(function() {
-     // Helper Function to Extract Access Token for URL
+$(document).ready(function() {
+    // Helper Function to Extract Access Token from URL
     const getUrlParameter = (sParam) => {
-      let sPageURL = window.location.search.substring(1),////substring will take everything after the https link and split the #/&
-          sURLVariables = sPageURL != undefined && sPageURL.length > 0 ? sPageURL.split('#') : [],
-          sParameterName,
-          i;
-      let split_str = window.location.href.length > 0 ? window.location.href.split('#') : [];
-      sURLVariables = split_str != undefined && split_str.length > 1 && split_str[1].length > 0 ? split_str[1].split('&') : [];
-      for (i = 0; i < sURLVariables.length; i++) {
-          sParameterName = sURLVariables[i].split('=');
-          if (sParameterName[0] === sParam) {
-              return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
-          }
-      }
-  };
+        let sPageURL = window.location.search.substring(1),
+            sURLVariables = sPageURL != undefined && sPageURL.length > 0 ? sPageURL.split('#') : [],
+            sParameterName,
+            i;
+        let split_str = window.location.href.length > 0 ? window.location.href.split('#') : [];
+        sURLVariables = split_str != undefined && split_str.length > 1 && split_str[1].length > 0 ? split_str[1].split('&') : [];
+        for (i = 0; i < sURLVariables.length; i++) {
+            sParameterName = sURLVariables[i].split('=');
+            if (sParameterName[0] === sParam) {
+                return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+            }
+        }
+    };
 
     // Get Access Token
     const accessToken = getUrlParameter('access_token');
 
-    // AUTHORIZE with Spotify (if needed)
+    // Authorize with Spotify (if needed)
     // *************** REPLACE THESE VALUES! *************************
     let client_id = '1ec7fd9573b54009b2fb16c6775d1231';
     // Use the following site to convert your regular url to the encoded version:
@@ -33,36 +32,21 @@ $( document ).ready(function() {
       window.location.replace(redirect);
     }
 
-    // Search button has been clicked
-    $( "#search_button" ).click(function() {
-      //Get the value of the search box
-      let raw_search_query = $('#search-text').val();
-      let search_query = encodeURI(raw_search_query);
-      // Make Spotify API call
-      // Note: We are using the track API endpoint.
-      $.ajax({
-        url: `https://api.spotify.com/v1/search?q=${search_query}&type=track`,
-        type: 'GET',
-        headers: {
-            'Authorization' : 'Bearer ' + accessToken
-        },
-        success: function(data) {
-          // Load our songs from Spotify into our page
-          let num_of_tracks = data.tracks.items.length;
-          let count = 0;
-          // Max number of songs is 12
-          const max_songs = 12;
-          while(count < max_songs && count < num_of_tracks){
-            // Extract the id of the FIRST song from the data object
-            let id = data.tracks.items[count].id;
-            // Constructing two different iframes to embed the song
-            let src_str = `https://open.spotify.com/embed/track/${id}`;
-            let iframe = `<div class='song'><iframe src=${src_str} frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe></div>`;
-            let parent_div = $('#song_'+ count);
-            parent_div.html(iframe);
-            count++;
-          }
-        }
-      }); // End of Spotify ajax call
-    }); // End of search button
-  }); // End of document.ready
+    // Function to Load Playlist from Spotify
+    const loadSpotifyPlaylist = () => {
+        $.ajax({
+            url: `https://api.spotify.com/v1/playlists/40WhYFDkKVUBwKPzVZOdPQ`,
+            headers: {
+                'Authorization': 'Bearer ' + accessToken
+            },
+            success: function(response) {
+                // Embed the playlist into the webpage
+                let playlistEmbedCode = `<iframe src="https://open.spotify.com/embed/playlist/40WhYFDkKVUBwKPzVZOdPQ" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`;
+                $('#spotify-playlist-container').html(playlistEmbedCode);
+            }
+        });
+    };
+
+    // Load the Spotify Playlist when the page is ready
+    loadSpotifyPlaylist();
+});
