5ecbbdc5aac4aedea6990bf0258088eb2c2af136
Trey <chadktracy@gmail.com>
2023-12-06 05:57:42 -0600

    redirect URI

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ $( document ).ready(function() {
     let client_id = '1ec7fd9573b54009b2fb16c6775d1231';
     // Use the following site to convert your regular url to the encoded version:
     // https://www.url-encode-decode.com/
-    let redirect_uri = 'https%3A%2F%2Fmujibsardar.github.io%2Fspotify_jquery_only'; // GitHub Pages URL or whatever your public url to this app is
+    let redirect_uri = 'https%3A%2F%2Fctrey2004.github.io%2Fspotify-demo'; // GitHub Pages URL or whatever your public url to this app is
     // *************** END *************************
 
     const redirect = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=token&redirect_uri=${redirect_uri}`;
